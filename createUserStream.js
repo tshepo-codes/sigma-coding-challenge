f70ac86a01d84e4533d19bf14f7a4ae6d5b637be
Tshepo Makhubela <tshepo@synthesis.co.za>
2022-08-22 10:02:20 +0200

    Setup dynamodb options for local db

--- createUserStream.js
+++ createUserStream.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+
+let dynamoOptions = {};
+
+if (process.env.IS_LOCAL) {
+  dynamoOptions = {
+    region: "localhost",
+    endpoint: "http://localhost:8000",
+  };
+}
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient(dynamoOptions);
+
+
 exports.hello = async (event) => {
 
 };
+
+
